cdfc5ea2c58ca182009a00f07fa7412e9e7ec64c
yaim0425 <yaim0425@gmail.com>
2025-09-19 22:13:37 -0500

    Creación de data-final-fixes.lua

---
+++ data-final-fixes.lua
@@ -0,0 +1,234 @@
+---------------------------------------------------------------------------
+---[ data-final-fixes.lua ]---
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Contenedor de este archivo ]---
+---------------------------------------------------------------------------
+
+local This_MOD = GMOD.get_id_and_name()
+if not This_MOD then return end
+GMOD[This_MOD.id] = This_MOD
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Inicio del MOD ]---
+---------------------------------------------------------------------------
+
+function This_MOD.start()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Valores de la referencia
+    This_MOD.setting_mod()
+
+    --- Obtener los elementos
+    This_MOD.get_elements()
+
+    -- --- Modificar los elementos
+    -- for iKey, spaces in pairs(This_MOD.to_be_processed) do
+    --     for jKey, space in pairs(spaces) do
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --         --- Marcar como procesado
+    --         This_MOD.processed[iKey] = This_MOD.processed[iKey] or {}
+    --         This_MOD.processed[iKey][jKey] = true
+
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --         -- --- Crear los elementos
+    --         -- This_MOD.create_subgroup(space)
+    --         -- This_MOD.create_item(space)
+    --         -- This_MOD.create_entity(space)
+    --         -- This_MOD.create_recipe(space)
+    --         -- This_MOD.create_tech(space)
+
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --     end
+    -- end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Valores de la referencia ]---
+---------------------------------------------------------------------------
+
+function This_MOD.setting_mod()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Validar si se cargó antes
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    if This_MOD.processed then return end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Valores de la referencia en todos los MODs
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Contenedor de los elementos que el MOD modoficó o modificará
+    This_MOD.to_be_processed = {}
+    This_MOD.processed = {}
+
+    --- Cargar la configuración
+    This_MOD.setting = GMOD.setting[This_MOD.id]
+
+    --- Indicador del mod
+    local Indicator = data.raw["virtual-signal"]["signal-heart"].icons[1].icon
+    This_MOD.indicator = { icon = Indicator, scale = 0.15, shift = { 0, -12 } }
+    This_MOD.indicator_bg = { icon = GMOD.color.black, scale = 0.15, shift = { 0, -12 } }
+    This_MOD.indicator_tech = { icon = Indicator, scale = 0.50, shift = { 0, -50 } }
+    This_MOD.indicator_tech_bg = { icon = GMOD.color.black, scale = 0.50, shift = { 0, -50 } }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Valores de la referencia en este MOD
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Daños a procesar
+    This_MOD.damages = {}
+    for damage, _ in pairs(data.raw["damage-type"]) do
+        table.insert(This_MOD.damages, damage)
+    end
+
+    --- Digitos necesarios para ordenar
+    This_MOD.damages_digits = GMOD.digit_count(#This_MOD.damages) + 1
+
+    --- Tipos a afectar
+    This_MOD.types = {
+        ["construction-robot"] = true,
+        ["logistic-robot"] = true
+    }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Funciones locales ]---
+---------------------------------------------------------------------------
+
+function This_MOD.get_elements()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Función para analizar cada entidad
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    local function valide(item, entity)
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Validación
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        --- Validar el item
+        if not item then return end
+
+        --- Validar el tipo
+        if not This_MOD.types[entity.type] then return end
+
+        --- Validar si ya fue procesado
+        if
+            This_MOD.processed[entity.type] and
+            This_MOD.processed[entity.type][item.name]
+        then
+            return
+        end
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Valores para el proceso
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        local Space = {}
+        Space.item = item
+        Space.entity = entity
+
+        Space.recipe = GMOD.recipes[Space.item.name]
+        Space.tech = GMOD.get_technology(Space.recipe)
+        Space.recipe = Space.recipe and Space.recipe[1] or nil
+
+        Space.prefix =
+            GMOD.name ..
+            (GMOD.get_id_and_name(entity.name) or { ids = "-" }).ids ..
+            This_MOD.id .. "-" .. entity.name .. "-"
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Guardar la información
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        This_MOD.to_be_processed[entity.type] = This_MOD.to_be_processed[entity.type] or {}
+        This_MOD.to_be_processed[entity.type][entity.name] = Space
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Buscar las entidades a afectar
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    for item_name, entity in pairs(GMOD.entities) do
+        valide(GMOD.items[item_name], entity)
+    end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Iniciar el MOD ]---
+---------------------------------------------------------------------------
+
+This_MOD.start()
+
+---------------------------------------------------------------------------
+GMOD.var_dump(This_MOD)
+ERROR()
